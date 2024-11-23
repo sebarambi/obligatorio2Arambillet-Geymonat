f6fa9ad3c01e34f3ca92e9f118dd5f6a715c5127
Sebarambi <sebarambi@yahoo.com>
2024-11-23 15:41:32 -0300

    inicio

---
+++ src/main/java/com/example/obligatorioDDA/ObligatorioDdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligatorioDDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatorioDdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatorioDdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/obligatorioDDA/ObligatorioDdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligatorioDDA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatorioDdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
